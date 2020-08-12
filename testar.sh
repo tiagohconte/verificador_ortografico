31a5a2d7f99863e6a483f8c1543754de86d17c7a
tiagohconte <tiagohconte@gmail.com>
2020-08-12 19:51:06 -0300

    Reorganização de arquivos, criação de script para testagem

---
+++ testar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ARQUIVOS=$(ls arq/)
+for i in $ARQUIVOS;
+do
+	echo -e "Rodando programa para $i..."
+	./ortografia < arq/$i > saida.txt
+	RES=$(diff saida.txt saidas-corretas/${i%.txt}-saida.txt)
+	if [ ! -z "$RES" ]
+	then
+		echo -e "$(tput setaf 1 && tput bold)ERRO! A saída não está correta para $i$(tput sgr 0)\n"
+	else
+		echo -e "SUCESSO!\n"
+	fi
+done
