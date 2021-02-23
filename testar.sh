1f88f7c1b2175b14a22a57bddb202e518172dcc9
Tiago Henrique Conte <tiagohconte@gmail.com>
2021-02-23 14:21:08 -0300

    Alterações no script de teste

--- testar.sh
+++ testar.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-ARQUIVOS=$(ls entradas/)
+ARQUIVOS=$(ls entradas)
+SAIDA="saidas"
+
+if [ ! -d "$SAIDA" ]; 
+then
+  mkdir $SAIDA
+fi
 
 for i in $ARQUIVOS;
 do
 	echo -e "Rodando programa para $i..."
-	time ./ortografia < entradas/$i > saida.txt
-	RES=$(diff saida.txt esperadas/${i%.txt}-saida.txt)
+
+    NEW_NAME=${i%.txt}.out
+	time ./ortografia < entradas/$i > $SAIDA/$NEW_NAME
+
+	RES=$(diff $SAIDA/$NEW_NAME esperadas/${i%.txt}-saida.txt)
+
 	if [ ! -z "$RES" ]
 	then
 		echo -e "$(tput setaf 1 && tput bold)ERRO! A saída não está correta para $i$(tput sgr 0)\n"
 	else
-		echo -e "SUCESSO!\n"
+		echo -e "$(tput setaf 2 && tput bold)SUCESSO!$(tput sgr 0)\n"
 	fi
 done
